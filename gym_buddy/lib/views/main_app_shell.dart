034b8748e1b14d410539ee793ee545f51f3df984
benadbro <13bridgeman.b@gmail.com>
2025-10-20 15:04:26 +0100

    Navbar changes

--- gym_buddy/lib/views/main_app_shell.dart
+++ gym_buddy/lib/views/main_app_shell.dart
@@ -22,28 +22,34 @@ class _AppShellState extends State<AppShell> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
-        backgroundColor: Colors.lightBlue,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.white,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: "Start Workout",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fitness_center),
-            label: "Exercises",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.access_time),
-            label: "History",
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-        ],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          splashFactory: NoSplash.splashFactory, // disables the white ripple
+          highlightColor: Colors.transparent, // removes highlight on tap
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentIndex,
+          onTap: (index) => setState(() => _currentIndex = index),
+          backgroundColor: const Color.fromARGB(255, 28, 34, 59),
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.white,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: "Start Workout",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.fitness_center),
+              label: "Exercises",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.access_time),
+              label: "History",
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          ],
+        ),
       ),
     );
   }
