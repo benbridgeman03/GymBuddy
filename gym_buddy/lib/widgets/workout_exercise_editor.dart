959fda8a98400cc7ce12d4ab35ce644b05d088dc
benadbro <13bridgeman.b@gmail.com>
2025-10-22 16:39:33 +0100

    Create workout_exercise_editor.dart
    Added workout exercise widget used for adding exercises to templates/workouts

---
+++ gym_buddy/lib/widgets/workout_exercise_editor.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WorkoutExerciseEditor extends StatefulWidget {
+  final String exerciseId;
+
+  const WorkoutExerciseEditor({super.key, required this.exerciseId});
+
+  @override
+  State<WorkoutExerciseEditor> createState() => _WorkoutExerciseEditor();
+}
+
+class _WorkoutExerciseEditor extends State<WorkoutExerciseEditor> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
